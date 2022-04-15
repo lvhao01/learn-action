5579b751ec9194e17c42a09467ce43bfec95c158
吕浩 <1274714546@qq.com>
2022-04-15 11:43:01 +0800

    fix:修复

--- env.js
+++ env.js
@@ -1,9 +1,11 @@
 const env = process.env || {};
-
+const FROM_EMAIL=env.FROM_EMAIL||'1274714546@qq.com';//发送邮箱
+const EMAIL_PASS=env.EMAIL_PASS||'wzcqokfloydkbadd';//发送邮箱的授权码
+const EMAIL_TO=env.EMAIL_TO;//发送给谁
 module.exports = {
-  FROM_EMAIL: env.FROM_EMAIL??'1274714546@qq.com',//发送邮箱
-  EMAIL_PASS: env.EMAIL_PASS??'wzcqokfloydkbadd',//发送邮箱的授权码
-  EMAIL_TO: env.EMAIL_TO,//发送给谁
+  FROM_EMAIL,
+  EMAIL_PASS,
+  EMAIL_TO,
 };
 
 
